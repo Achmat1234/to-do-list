7260783baafce2e7e87aedf4717cfdd48a913297
Achmat <achmat.zaydan@gmail.com>
2019-09-03 13:39:24 +0200

    changed image

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (isset($_GET['to'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/styling.css" rel="stylesheet" type="text/css">
     <title>THINGS TO DO</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
